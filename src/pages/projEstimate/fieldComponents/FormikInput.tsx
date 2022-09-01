f12dbf6d062c3bc47700887ed0bf1a4836a1b939
Lorenz Ras <lenzras@gmail.com>
2022-09-01 10:36:25 +0900

    Update FormikInput.tsx

--- src/pages/projEstimate/fieldComponents/FormikInput.tsx
+++ src/pages/projEstimate/fieldComponents/FormikInput.tsx
@@ -1,23 +1,37 @@
-import { debounce, FormControl, FormHelperText, Input } from '@mui/material';
+import { FormControl, FormHelperText, Input } from '@mui/material';
 import { useField } from 'formik';
+import { useState } from 'react';
+import { useLazyEffect } from '../../../hooks';
 
 export const FormikInput = (
   { name }:
   { name: string },
 ) => {
   const [field, meta, helpers] = useField(name);
+  const [value, setValue] = useState<string>(field.value ?? '');
   const { error, touched } = meta;
-  const changeHandlerInput
-  : React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined
-   = debounce((el) => {
-     console.log('2s after chk', el.target.value);
 
-     helpers.setValue(el.target.value, true);
-   }, 2000);
+  const changeHandlerInput : React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined
+   = (el) => {
+
+     setValue(el.target.value);
+   };
+
+  useLazyEffect(()=>{
+
+    helpers.setValue(value);
+
+  }, [value], 1000);
+
 
   return (
     <FormControl variant="standard">
-      <Input {...field} error={!!error && touched} onChange={changeHandlerInput} />
+      <Input
+        {...field}
+        error={!!error && touched}
+        onChange={changeHandlerInput}
+        value={value}
+        />
       {(!!error && touched) &&
       <FormHelperText error={!!error && touched}>
         {error}
